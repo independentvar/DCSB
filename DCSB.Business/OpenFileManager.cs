8410495efba650332607bee64d544719580776ef
independentvar <martynas.lobinas@gmail.com>
2026-07-06 22:34:23 +0200

    Feature: backup and restore (#49)

--- DCSB.Business/OpenFileManager.cs
+++ DCSB.Business/OpenFileManager.cs
@@ -1,9 +1,50 @@
 ﻿using Microsoft.Win32;
+using System;
 
 namespace DCSB.Business
 {
     public class OpenFileManager
     {
+        public string SaveBackupFile()
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog
+            {
+                Title = "Save settings backup",
+                Filter = "DCSB backup (*.xml)|*.xml",
+                AddExtension = true,
+                DefaultExt = "xml",
+                FileName = "DCSB_backup_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".xml",
+                RestoreDirectory = true
+            };
+
+            bool? result = fileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                return fileDialog.FileName;
+            }
+            return null;
+        }
+
+        public string OpenBackupFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Title = "Choose settings backup",
+                Filter = "DCSB backup (*.xml)|*.xml|all files (*.*)|*.*",
+                RestoreDirectory = true,
+                Multiselect = false
+            };
+
+            bool? result = fileDialog.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                return fileDialog.FileName;
+            }
+            return null;
+        }
+
         public string OpenCounterFile()
         {
             OpenFileDialog fileDialog = new OpenFileDialog
